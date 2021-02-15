040bf0116fea256b38d0ac9351933cba460f0305
Luis E Lozano <lozano_22_lalo@yahoo.com>
2021-02-15 01:25:34 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,7 +2,15 @@ require 'yaml'
 
 def load_library(file)
   emoticons_library = YAML.load_file(file)
-  language_key = {}
+  #loads emojis to library
+  language_key = {} #initializes a has to store the formatted the library information
+  
+  emoticons_library.each do |key, value|
+    #iterates a hash to store the formatted library information.
+    hash_library[key] = {}
+    hash_library[key][:english] = value[0]
+    
+  end
   
 
 end
