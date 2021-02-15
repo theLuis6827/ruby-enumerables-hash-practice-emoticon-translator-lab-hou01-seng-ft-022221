d5d30f725e6c31b3ec615e18bb0b283f6e468aab
Luis E Lozano <lozano_22_lalo@yahoo.com>
2021-02-15 00:52:22 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,20 +1,7 @@
 require 'yaml'
 
 def load_library
-  emoticons = YAML.load_file(path)
-  lookup = {}
-  lookup[:get_meaning] = {}
-  lookup[:get_emoticon] = {}
 
-  emoticons.each do |word, pair|
-    japanese = pair[1]
-    american = pair[0]
-
-    lookup[:get_meaning][japanese] = word
-    lookup[:get_emoticon][american] = japanese
-  end
-
-  lookup
 end
 
 def get_english_meaning
